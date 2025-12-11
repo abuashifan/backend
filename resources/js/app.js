b22559bf0f6fd663f6220e42b5930f31eeddc4c9
wardi <alifashifan@gmail.com>
2025-12-12 01:46:23 +0700

    revisi vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 import './bootstrap';
 
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { createInertiaApp } from '@inertiajs/vue3';
 
 createInertiaApp({
-	resolve: (name) =>
-		import(`./Pages/${name}.vue`).then((module) => module.default),
-	setup({ el, App, props, plugin }) {
-		createApp({ render: () => h(App, props) }).use(plugin).mount(el);
-	},
+    resolve: name => import(`./Pages/${name}.vue`),
+    setup({ el, App, props }) {
+        createApp({ render: () => h(App, props) })
+            .mount(el);
+    },
 });
